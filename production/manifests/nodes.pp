4dcef1bb97c21e6726e2e626c84b19282afe6783
skc9999 <saikrishnapapineni9999@gmail.com>
2016-09-08 13:33:22 -0500

    Update nodes.pp

--- production/manifests/nodes.pp
+++ production/manifests/nodes.pp
@@ -18,6 +18,11 @@ file { '/tmp/ubu':
         content=>'again created node.pp !!'
 
 }
+
+file { '/tmp/ubu2':
+        content=>'again and again created node.pp !!'
+
+}
 package { 'elinks':
   ensure => present,}
 
